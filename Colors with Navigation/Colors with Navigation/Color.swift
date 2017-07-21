b67a05ce969b7bc05a97f149eec2e4f3e2d99ccf
jamiefloress <jamiefloress456@gmail.com>
2017-07-21 03:16:19 -0500

    Added Color struct and set background color on cells.

---
+++ Colors with Navigation/Colors with Navigation/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Colors with Navigation
+//
+//  Created by Jamie Flores on 7/21/17.
+//  Copyright © 2017 Jamie Flores. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
